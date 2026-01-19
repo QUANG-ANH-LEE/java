b87ff56d43979d22a079c0a0a93d13aeb71d9290
LeNangQuangAnh <quanganhlenang@gmail.com>
2026-01-19 13:26:25 +0700

    complete assigment 2.2.5

---
+++ lab01/calculator.java
@@ -0,0 +1,21 @@
+// Example 2.2.5
+import javax.swing.JOptionPane;
+public class calculator {
+    public static void main(String[] args){
+        String strNum1, strNum2;
+
+
+        strNum1 = JOptionPane.showInputDialog(null, "Please input the first number: ", "Input the first number", JOptionPane.INFORMATION_MESSAGE);
+        double num1 = Double.parseDouble(strNum1);
+
+        strNum2 = JOptionPane.showInputDialog(null, "Please input the second number: ", "Input the second number", JOptionPane.INFORMATION_MESSAGE);
+        double num2 = Double.parseDouble(strNum2);
+
+        JOptionPane.showMessageDialog(null, "a + b = " + (num1 + num2), "Sum", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "a - b = " + (num1 - num2), "Substract", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "a * b = " + (num1 * num2), "Product", JOptionPane.INFORMATION_MESSAGE);
+        if(num2 != 0)
+            JOptionPane.showMessageDialog(null, "a / b = " + (num1 / num2), "Divide", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
+}
